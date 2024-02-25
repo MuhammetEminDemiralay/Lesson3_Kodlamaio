f1d8d1d3b2d7ee0c44273afefd2ebe2e1d5482c3
Muhammet Emin Demiralay <pepmuhammet.42@gmail.com>
2024-02-25 22:30:21 +0300

    Lesson3 finished.

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,3 +1,8 @@
+using Business.Abstracts;
+using Business.Concretes;
+using DataAccess.Abstracts;
+using DataAccess.Concretes;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<IBrandDal, BrandDal>();
+builder.Services.AddSingleton<IBrandService, BrandManager>();
+
 
 var app = builder.Build();
 
